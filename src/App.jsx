98cd2804fac0a244212a39441bfac7e8f5053d83
soulfulDreamer <kazishahhamza@outlook.com>
2024-11-05 08:44:52 +0600

    database added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useRef, useState } from "react";
+import "./App.css";
 import { Auth } from "./components/Auth";
+import Cookies from "universal-cookie";
+import Chat from "./components/Chat";
+const cookies = new Cookies();
 
 const App = () => {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {
+    return (
+      <>
+        <Auth setIsAuth={setIsAuth} /> 
+      </>
+    );
+  }
+
   return (
     <>
-      <Auth />
+      {room ? (
+        <Chat />
+      ) : (
+        <div className="room">
+          <label>Enter Room Name:</label>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}> 
+            Enter Chat
+          </button>
+        </div>
+      )}
     </>
   );
 };
